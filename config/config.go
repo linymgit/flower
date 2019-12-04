59e744c9b7d4e9fd298bfc44b7eed904d884522e
linym <18316471919@163.com>
2019-12-04 22:33:21 +0800

    feat: add config

--- config/config.go
+++ config/config.go
@@ -1,5 +1,35 @@
 package config
 
-const News_Type_Id  = 37
+const News_Type_Id = 37
 
+type Config struct {
+	NewsTypeId    int           `json:"news_type_id"`
+	Md5Salt       string        `json:"md5_salt"`
+	CaptchaConfig CaptchaConfig `json:"captcha_config"`
+	JwtConfig     JwtConfig     `json:"jwt_config"`
+	QNconfig      QNconfig      `json:"qn_config"`
+	CorsConfig    CorsConfig    `json:"cors_config,omitempty"`
+}
 
+type CaptchaConfig struct {
+	CaptchaHeight     int `json:"captcha_height"`
+	CaptchaWidth      int `json:"captcha_weight"`
+	CaptchaExpiredMin int `json:"captcha_expired_min"`
+}
+
+type JwtConfig struct {
+	JwtSecretKey  string `json:"jwt_secret_key"`
+	JwtExpiredMin int    `json:"jwt_expired_min"`
+}
+
+type QNconfig struct {
+	AccessKey string `json:"access_key"`
+	SecretKey string `json:"secret_key"`
+}
+
+type CorsConfig struct {
+	CorsAllowHeaders     string `json:"cors_allow_headers"`
+	CorsAllowMethods     string `json:"cors_allow_methods"`
+	CorsAllowOrigin      string `json:"cors_allow_origin"`
+	CorsAllowCredentials string `json:"cors_allow_credentials"`
+}
