0cd9a2eea1dfed963386c7b0877d4ea7a9737996
18316471919 <18316471919@139.com>
2019-12-04 13:48:58 +0800

    fix: 跨域优化

--- handler/options_handler.go
+++ handler/options_handler.go
@@ -1,16 +1,8 @@
 package handler
 
 import (
-	"encoding/json"
-	"flower/result"
 	"github.com/valyala/fasthttp"
 )
 
 var Options = CROS(func(ctx *fasthttp.RequestCtx) {
-	success := result.NewSuccess("")
-	bytes, e := json.Marshal(success)
-	if e != nil {
-		//todo
-	}
-	ctx.Response.SetBodyRaw(bytes)
 })
